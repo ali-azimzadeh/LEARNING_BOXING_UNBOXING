96e13fdc914b2b77defd7115b0b6ed83a6ea87e8
ali azimzadeh <aliazimzadeh@gmail.com>
2022-08-27 10:49:47 +0430

    first commit

---
+++ LEARNING_BOXING_UNBOXING/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEARNING_BOXING_UNBOXING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BOXING
+
+            int test = 90; //value type
+            object myObject = test; // test is boxed to myObject
+
+            Console.WriteLine(myObject.ToString());
+
+
+            //UNBOXING
+
+            int foo = 74; // Value type
+            object bar = foo; // foo is boxed to bar
+            int foo۲ = (int)bar; // Unboxed back to value type
+
+            Console.WriteLine(foo۲.ToString());
+
+
+            System.Console.ReadKey();
+        }
+    }
+}
